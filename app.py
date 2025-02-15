4788a6c84dca1c98188c55aa27178d21a82085b7
Ninah Fukami <nifukami@gmail.com>
2025-02-14 20:19:30 -0500

    Tictactoe Project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def reset():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
